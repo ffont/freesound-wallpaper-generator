5cfcec5bd6e091d61459d5134c4e4826b234d375
ffont <frederic.font@upf.edu>
2018-06-25 11:54:38 +0200

    DEBUG mode env var must be '1' for True

--- code/app.py
+++ code/app.py
@@ -13,7 +13,7 @@ socketio = SocketIO(app)
 
 HOST = os.getenv('HOST', '0.0.0.0')
 PORT = os.getenv('PORT', 5000)
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv('DEBUG', '1') == '1'  # Set it to '1' for DEBUG mode True, otherwise will be False
 BASE_URL = os.getenv('BASE_URL', 'http://localhost:5000/')
 DATA_DIR = os.getenv('DATA_DIR', '/code/data/')
 
